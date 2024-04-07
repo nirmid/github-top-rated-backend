b0ad98a4e53cacae3710e95e4fdfc9b89864ddf7
nirmid <93215733+nirmid@users.noreply.github.com>
2024-04-07 16:44:08 +0300

    Add Dockerfile and .dockerignore, update package.json and routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,43 +3,6 @@ import { authController } from "../controllers/auth";
 
 export const authRoutes = express.Router();
 
-/**
- * @openapi
- * /healthcheck:
- *  get:
- *     tags:
- *     - Healthcheck
- *     description: Responds if the app is up and running
- *     responses:
- *       200:
- *         description: App is up and running
- */
 authRoutes.post("/login", authController.login);
 
-/**
- * @openapi
- * /auth/signup:
- * put:
- * summary: Signup
- * description: Signup
- * requestBody:
- * required: true
- * content:
- * application/json:
- * schema:
- * type: object
- * properties:
- * username:
- * type: string
- * password:
- * type: string
- * required:
- * - username
- * - password
- * responses:
- * 201:
- * description: Created
- *
- *
- **/
 authRoutes.put("/signup", authController.signup);
